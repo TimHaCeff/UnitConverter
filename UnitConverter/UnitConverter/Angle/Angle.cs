888a14fc33e63239f2b91179f1dbaf65d9996c95
TimHaCeff <Tim.ha@ceff.ch>
2024-08-26 10:44:28 +0200

    add first Angle.cs

---
+++ UnitConverter/UnitConverter/Angle/Angle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitConverter.Angle
+{
+    public class Angle
+    {
+        public enum AngleTypeEnum 
+        {
+            RAD,
+            DEG
+        }
+        protected double _angle { get; set; }
+
+        public AngleTypeEnum Type { get; set; }
+
+        public Angle(double angle, AngleTypeEnum angleType)
+        {
+            _angle = angle;
+            this.Type = angleType;
+        }
+
+        public double 
+    }
+}
--- UnitConverter/UnitConverter/UnitConverter.cs
+++ UnitConverter/UnitConverter/UnitConverter.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnitConverter.Angle;
 
 namespace UnitConverter
 {
     public class UnitConverter
     {
-
+        Angle.Angle angle = new Angle.Angle(20, Angle.Angle.AngleTypeEnum.RAD);
+        public void Test() 
+        {
+        }
     }
 }
